bc3dbb5b67f1e016c141272dca270897e232b01f
Kevin Allard <contact@allard-kevin.fr>
2017-12-04 12:53:24 +0100

    WIP

---
+++ src/AppBundle/Controller/Admin/CategoryAdminController.php
@@ -0,0 +1,29 @@
+<?php
+
+namespace AppBundle\Controller\Admin;
+
+use Sonata\AdminBundle\Admin\AbstractAdmin;
+use Sonata\AdminBundle\Datagrid\DatagridMapper;
+use Sonata\AdminBundle\Datagrid\ListMapper;
+use Sonata\AdminBundle\Form\FormMapper;
+
+class CategoryAdminController extends AbstractAdmin
+{
+    protected function configureFormFields(FormMapper $formMapper)
+    {
+        $formMapper
+            ->add('name', 'text')
+            ->add('description', 'textarea')
+            ->add('status', 'checkbox');
+    }
+
+    protected function configureDatagridFilters(DatagridMapper $datagridMapper)
+    {
+        $datagridMapper->add('name');
+    }
+
+    protected function configureListFields(ListMapper $listMapper)
+    {
+        $listMapper->addIdentifier('name');
+    }
+}
