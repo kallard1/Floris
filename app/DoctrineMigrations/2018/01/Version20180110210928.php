8bd657a4527f5c598929d7d9a789ba100b902d50
Kevin Allard <contact@allard-kevin.fr>
2018-01-11 08:42:39 +0100

    Migrations

---
+++ app/DoctrineMigrations/2018/01/Version20180110210928.php
@@ -0,0 +1,31 @@
+<?php declare(strict_types = 1);
+
+namespace Application\Migrations;
+
+use Doctrine\DBAL\Migrations\AbstractMigration;
+use Doctrine\DBAL\Schema\Schema;
+
+/**
+ * Auto-generated Migration: Please modify to your needs!
+ */
+class Version20180110210928 extends AbstractMigration
+{
+    public function up(Schema $schema)
+    {
+        // this up() migration is auto-generated, please modify it to your needs
+        $this->abortIf($this->connection->getDatabasePlatform()->getName() !== 'postgresql', 'Migration can only be executed safely on \'postgresql\'.');
+
+        $this->addSql('ALTER TABLE vat_rate ALTER rate TYPE DOUBLE PRECISION');
+        $this->addSql('ALTER TABLE vat_rate ALTER rate DROP DEFAULT');
+    }
+
+    public function down(Schema $schema)
+    {
+        // this down() migration is auto-generated, please modify it to your needs
+        $this->abortIf($this->connection->getDatabasePlatform()->getName() !== 'postgresql', 'Migration can only be executed safely on \'postgresql\'.');
+
+        $this->addSql('CREATE SCHEMA public');
+        $this->addSql('ALTER TABLE vat_rate ALTER rate TYPE SMALLINT');
+        $this->addSql('ALTER TABLE vat_rate ALTER rate DROP DEFAULT');
+    }
+}
