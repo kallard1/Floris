60371d9666d192f4165a8aacb01e42aef9fff19f
Kevin Allard <contact@allard-kevin.fr>
2018-01-14 16:52:22 +0100

    Création entité plus liaison avec Customer

--- src/AppBundle/Entity/Cart.php
+++ src/AppBundle/Entity/Cart.php
@@ -3,13 +3,14 @@
 namespace AppBundle\Entity;
 
 use Doctrine\ORM\Mapping as ORM;
-use Doctrine\ORM\Mapping\JoinColumn;
+use Gedmo\Mapping\Annotation as Gedmo;
 
 /**
  * Class Cart
  *
  * @package AppBundle\Entity
  * @ORM\Table(name="cart")
+ * @ORM\Entity(repositoryClass="AppBundle\Repository\CartRepository")
  */
 class Cart
 {
@@ -25,9 +26,32 @@ class Cart
     /**
      * One Cart has One Customer.
      * @ORM\OneToOne(targetEntity="Customer", inversedBy="cart")
-     * @JoinColumn(name="customer_id", referencedColumnName="id")
+     * @ORM\JoinColumn(name="customer_id", referencedColumnName="id")
      */
     private $customer;
+
+    /**
+     * @var array
+     *
+     * @ORM\Column(name="product", type="json_array")
+     */
+    private $product;
+
+    /**
+     * @var \DateTime $createdAt
+     *
+     * @Gedmo\Timestampable(on="create")
+     * @ORM\Column(type="datetime")
+     */
+    private $createdAt;
+
+    /**
+     * @var \DateTime $updatedAt
+     *
+     * @Gedmo\Timestampable(on="update")
+     * @ORM\Column(type="datetime")
+     */
+    private $updatedAt;
 
     /**
      * @return int
@@ -51,5 +75,77 @@ class Cart
     public function setCustomer($customer)
     {
         $this->customer = $customer;
+    }
+
+    /**
+     * Set product.
+     *
+     * @param array $product
+     *
+     * @return Cart
+     */
+    public function setProduct($product)
+    {
+        $this->product = $product;
+
+        return $this;
+    }
+
+    /**
+     * Get product.
+     *
+     * @return array
+     */
+    public function getProduct()
+    {
+        return $this->product;
+    }
+
+    /**
+     * Set createdAt.
+     *
+     * @param \DateTime $createdAt
+     *
+     * @return Cart
+     */
+    public function setCreatedAt($createdAt)
+    {
+        $this->createdAt = $createdAt;
+
+        return $this;
+    }
+
+    /**
+     * Get createdAt.
+     *
+     * @return \DateTime
+     */
+    public function getCreatedAt()
+    {
+        return $this->createdAt;
+    }
+
+    /**
+     * Set updatedAt.
+     *
+     * @param \DateTime $updatedAt
+     *
+     * @return Cart
+     */
+    public function setUpdatedAt($updatedAt)
+    {
+        $this->updatedAt = $updatedAt;
+
+        return $this;
+    }
+
+    /**
+     * Get updatedAt.
+     *
+     * @return \DateTime
+     */
+    public function getUpdatedAt()
+    {
+        return $this->updatedAt;
     }
 }
