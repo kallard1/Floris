fce637d50cf467df122e89c5ca0ca10ec450a2ff
Kevin Allard <contact@allard-kevin.fr>
2018-03-10 18:03:29 +0100

    Modification du controlleur panier

--- src/AppBundle/Controller/CartController.php
+++ src/AppBundle/Controller/CartController.php
@@ -2,40 +2,97 @@
 
 namespace AppBundle\Controller;
 
-use AppBundle\Entity\Cart;
 use AppBundle\Entity\Product;
 use Sensio\Bundle\FrameworkExtraBundle\Configuration\ParamConverter;
-use Sensio\Bundle\FrameworkExtraBundle\Configuration\Route;
 use Symfony\Bundle\FrameworkBundle\Controller\Controller;
 use Symfony\Component\HttpFoundation\Request;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Annotation\Route;
 
 class CartController extends Controller
 {
     /**
-     * @Route("/cart/{sku}/add", name="add_to_cart")
-     * @ParamConverter("product", class="AppBundle\Entity\Product", options={"sku" = "sku"})
+     * @Route("/cart/add/{id}", name="add_to_cart")
+     * @ParamConverter("product", class="AppBundle:Product")
      * @param Request $request
      * @param Product $product
+     * @return Response
      */
-    public function addToCartAction(Request $request, Product $product)
+    public function addAction(Request $request, Product $product)
     {
-        $manager = $this->getDoctrine()->getManager();
-
-        $currentUser = $this->get('security.token_storage')->getToken()->getUser();
-
-        $cart = $manager->getRepository('AppBundle:Cart');
-
-        $userCart = $cart->findOneBy(['customer' => $currentUser->getId()]);
-
-        if (!$userCart) {
-            $newCart = new Cart();
-            $newCart->setCustomer($currentUser);
-
-            $manager->persist($newCart);
-            $manager->flush();
+        $quantity = $request->query->get('quantity', 1);
+        if ($quantity < 1) {
+            return new Response('Quantitée négative ou de 0', '400');
         }
-        $userCart->addProduct($product);
-        $manager->persist($userCart);
-        $manager->flush();
+        $session   = $request->getSession();
+        $products  = $session->get('products', []);
+        $found     = false;
+        $promotion = $product->getPromotion() == null ? null : $product->getPromotion();
+        foreach ($products as $key => $value) {
+            if ($product->getId() === $value['product']->getId()) {
+                $products[$key]['quantity'] += $quantity;
+                $found                      = true;
+            }
+        }
+        if ($found == false) {
+            array_push($products, ['product' => $product, 'quantity' => $quantity, 'promotion' => $promotion]);
+        }
+        $session->set('products', $products);
+        return new Response();
+    }
+
+    /**
+     * @param Request $request
+     * @return int|Response
+     */
+    public function listAction(Request $request)
+    {
+        $products = $request->getSession()->get('products', []);
+        $total    = 0;
+        if ($request->isXmlHttpRequest()) {
+            $count = 0;
+            foreach ($products as $product) {
+                $count += $product['quantity'];
+            }
+            return new Response($count);
+        }
+        foreach ($products as $list) {
+            /** @var Product $product */
+            $product = $list['product'];
+            $total   += ($product->getPriceHt() * $list['quantity']);
+            if ($product->getPromotion() !== null) {
+                $total -= ($product->getPromotion()->getTaux() / 100) * $total;
+            }
+            $total = number_format($total, 2);
+        }
+        return $this->render('AppBundle:Cart:list.html.twig', [
+            'products' => $products,
+            'total' => $total
+        ]);
+    }
+
+    /**
+     * @ParamConverter("product", class="AppBundle:Product")
+     * @param Request $request
+     * @param Product $product
+     * @return Response
+     * @throws EntityNotFoundException
+     */
+    public function deleteAction(Request $request, Product $product)
+    {
+        $session  = $request->getSession();
+        $products = $session->get('products', []);
+        $found    = false;
+        foreach ($products as $key => $value) {
+            if ($product->getId() === $value['product']->getId()) {
+                unset($products[$key]);
+                $found = true;
+            }
+        }
+        if ($found == false) {
+            throw new EntityNotFoundException('La ressource demandée n\'existe pas dans le panier.');
+        }
+        $session->set('products', $products);
+        return $this->redirectToRoute('app_cart_see');
     }
 }
