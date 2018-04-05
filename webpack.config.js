c83bb4cb0cbb4f350d0d65ab76840db70eac6ff8
Kevin Allard <contact@allard-kevin.fr>
2018-04-05 11:14:45 +0200

    Optimisation

--- webpack.config.js
+++ webpack.config.js
@@ -1,4 +1,4 @@
-var Encore = require('@symfony/webpack-encore')
+let Encore = require('@symfony/webpack-encore')
 
 Encore
   .setOutputPath('web/build/')
@@ -10,11 +10,9 @@ Encore
     jQuery: 'jquery'
   })
   .enableSassLoader()
-  .enableVersioning(false) // hashed filenames (e.g. main.abc123.js)
-  .createSharedEntry('js/common', ['jquery'])
+  .enableVersioning(true) // hashed filenames (e.g. main.abc123.js)
   .addEntry('js/app', './assets/js/app.js')
   .addStyleEntry('css/global', './assets/sass/app.scss')
   .addStyleEntry('css/dataTables.bootstrap4', './node_modules/datatables.net-bs4/css/dataTables.bootstrap4.css')
-
 
 module.exports = Encore.getWebpackConfig()
